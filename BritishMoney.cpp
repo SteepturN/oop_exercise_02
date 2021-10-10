999dce8709c938e5a4b390162d64f003971e80c1
admin <admin@gg>
2021-10-10 19:00:07 +0300

    improved input

--- BritishMoney.cpp
+++ BritishMoney.cpp
@@ -1,46 +1,44 @@
-
+#include <cmath>
+#include <iostream>
+//1 фунт = 20 шиллингов, 1 шиллинг = 12 пенни.
 class BritishMoney{
 	public:
 	BritishMoney(unsigned long long funt, unsigned char shil, unsigned char penny)
 	:	funt(funt), shil(shil), penny(penny) {};
-	private:
-	unsigned long long funt;
-	unsigned char shil;
-	unsigned char penny;
 	//1 фунт = 20 шиллингов, 1 шиллинг = 12 пенни
 	BritishMoney operator+(const BritishMoney& bm) const noexcept{
 		int conv_penny = (penny+bm.penny)/12;
 		int _penny = (penny+bm.penny)%12;
 		int conv_shil = (shil+bm.shil+conv_penny)/20;
 		int _shil = (shil+bm.shil+conv_penny)%20;
-		int _funt = funt+bm.funt+conv_shil;
+		unsigned long long _funt = funt+bm.funt+conv_shil;
 		return BritishMoney(_funt, _shil, _penny);
 	}
 	BritishMoney operator-(const BritishMoney& bm) const noexcept{
-		int k = bm<this*? 1 : -1;
-		int _funt = (funt-bm.funt)*k;
+		int k = bm < (*this) ? 1 : 0;
+		unsigned long long _funt = (funt-bm.funt)*k;
 		int _shil = (shil-bm.shil)*k;
 		int _penny = (penny-bm.penny)*k;
-		if(_penny < 0) _shil-=1; _penny+=12;
-		if(_shil < 0) _funt-=1; _shil+=20;
-		return BritishMoney(_funt*k, _shil*k, _penny*k);
+		if(_penny < 0) {_shil-=1; _penny+=12;}
+		if(_shil < 0) {_funt-=1; _shil+=20;}
+		return BritishMoney(_funt, _shil, _penny);
 	}
 	double operator/(const BritishMoney& bm) const noexcept{
 		return (funt+shil/12.0+penny/240.0)/(bm.funt+bm.shil/12.0+bm.penny/240.0);
 	}
-	BritishMoney operator/(double& num) const noexcept{
-		long double res = (funt+shil/12.0+penny/240.0)/num;
-		unsigned long long _funt = trunc(value);
-		unsigned char _shil = trunc((res-funt)*12);
-		unsigned char _penny = trunc(((res-funt)*12-_shil)*20);
+	BritishMoney operator/(const long double& num) const noexcept{
+		if(num == 0) return BritishMoney(0, 0, 0);
+		long double res = (funt+shil/12.0L+penny/240.0L)/num;
+		unsigned long long _funt = trunc(res);
+		unsigned char _shil = static_cast<unsigned char>(trunc((res-_funt)*20));
+		unsigned char _penny = static_cast<unsigned char>(trunc(((res-_funt)*20-_shil)*12));
 		return BritishMoney(_funt, _shil, _penny);
 	}
-	BritishMoney operator*(double& num) const noexcept{
-		int conv_penny = (penny*num)/12;
-		int _penny = (penny*num)%12;
-		int conv_shil = (shil*num+conv_penny)/20;
-		int _shil = (shil*num+conv_penny)%20;
-		int _funt = funt*num+conv_shil;
+	BritishMoney operator*(const long double& num) const noexcept{
+		long double res = (funt+shil/12.0L+penny/240.0L)*num;
+		unsigned long long _funt = trunc(res);
+		unsigned char _shil = static_cast<unsigned char>(trunc((res-_funt)*20));
+		unsigned char _penny = static_cast<unsigned char>(trunc(((res-_funt)*20-_shil)*12));
 		return BritishMoney(_funt, _shil, _penny);
 	}
 	bool operator<(const BritishMoney& bm) const noexcept{
@@ -48,9 +46,25 @@ class BritishMoney{
 		else if(funt == bm.funt){
 			if(shil < bm.shil) return true;
 			else if(shil == bm.shil)
-				return penny < penny.bm;
+				return penny < bm.penny;
 			else return false;
 		}
 		else return false;
 	}
-}
+	void print(){
+		std::cerr << "фунты = " << funt << ", шиллинги = " << static_cast<int>(shil) << ", пенсы = " << static_cast<int>(penny);
+	}
+	BritishMoney exec(const char* command, const BritishMoney& bm) const noexcept {
+		if(static_cast<std::string>(command) == "a") return *this+bm;
+		else if(static_cast<std::string>(command) == "s") return *this-bm;
+		else return BritishMoney(0,0,0);
+	}
+	BritishMoney exec(const char* command, const double koef) const noexcept {
+		if(static_cast<std::string>(command) == "dbn") return *this/koef;
+		else if(static_cast<std::string>(command) == "m") return *this*koef;
+		else return BritishMoney(0,0,0);
+	}
+	unsigned long long funt;
+	unsigned char shil;
+	unsigned char penny;
+};
--- main.cpp
+++ main.cpp
@@ -6,55 +6,111 @@
 -типа unsigned char – для шиллингов,
 -unsigned char – для пенсов (пенни).
 Реализовать
-сложение сумм, вычитание, деление сумм, деление суммы на дробное число, умножение на дробное число и операции сравнения.
+сложение сумм,
+вычитание,
+деление сумм,
+деление суммы на дробное число,
+умножение на дробное число и
+операции сравнения.
 1 фунт = 20 шиллингов, 1 шиллинг = 12 пенни.
 */
 #include <iostream>
-#include <cstring>
-#include "Complex.cpp"
+#include <string>
+#include "BritishMoney.cpp"
+#include "Read_input.cpp"
 #include <cstdio>
+#include <set>
 int main(){
-
-	double re1(0), im1(0), re2(0), im2(0);
+	const char a[] = "You can use\n\
+--add: a (funt1) (shilling1) (penny1) (funt2) (shilling2) (penny2)\n\
+--subtraction: s (funt1) (shilling1) (penny1) (funt2) (shilling2) (penny2)\n\
+--division of sums: d (funt1) (shilling1) (penny1) (funt2) (shilling2) (penny2)\n\
+--sum1 < sum2: lth (funt1) (shilling1) (penny1) (funt2) (shilling2) (penny2)\n\n\
+--divide by number: dbn (funt1) (shilling1) (penny1) (real number)\n\
+--multiplication by number: m (funt1) (shilling1) (penny1) (real number)\n\n\
+--exit\n";
+	long double input_real_number(0.0);
+	unsigned long long funt1n(0), funt2n(0); unsigned int shil1n(0), shil2n(0), penny1n(0), penny2n(0);
+	int conv_penny(0), conv_shil(0);
+	BritishMoney bm1(0,0,0);
+	BritishMoney bm2(0,0,0);
+	
 	char ch(' ');
 	char command[20];
-	do{
-		do ch = getchar(); while(ch == ' ' || ch == '\n' || ch == '\t');
-		if(ch == EOF) return 0;
-		else ungetc(ch, stdin);
-		scanf("%s", command);
-		if(strcmp(command,"c")==0){
-			scanf("%lf%lf", &re1, &im1);
-			std::cout << "conj" << '(' << re1 << ',' << im1 << ')' << " = ";
-			Complex(re1, im1).conj().print();
+	bool exit;
+	std::set<std::string> valid_commands = {"a", "s", "d", "dbn", "m", "lth", "exit"};
+	std::cout << a;
+	do {
+		exit = false;
+		do{
+			read_return_t answer = get_command(valid_commands, command);
+			switch(answer) {
+				case END_OF_FILE: return 0;
+				case END_OF_LINE: continue;
+				case VALID_INPUT: exit = true; break;
+				case INVALID_INPUT:
+					do ch=getchar(); while((ch != EOF) && (ch != '\n'));
+					std::cout << "wrong input" << std::endl;
+					if(ch == EOF) return 0;
+					else break;
+				}
+		} while(!exit);
+		if(static_cast<std::string>(command) == "exit") return 0;
+		if((get_value<unsigned long long>(funt1n) != VALID_INPUT) || (get_value<unsigned int>(shil1n) != VALID_INPUT) || (get_value<unsigned int>(penny1n) != VALID_INPUT)) {
+			do ch = getchar(); while((ch != EOF) && (ch != '\n'));
+			std::cout << "wrong input" << std::endl;
+			if(ch == EOF) return 0;
+			continue;
 		}
-		else if(strcmp(command, "exit")==0) return 0;
-		else{
-			scanf("%lf%lf%lf%lf", &re1, &im1, &re2, &im2);
-			std::cout << '(' << re1 << ',' << im1 << ')' << command << '(' << re2 << ',' << im2 << ')' << " = ";
-			if(strcmp(command,"a")==0){
-				Complex(re1, im1).add(Complex(re2, im2)).print();
-			}
-			else if(strcmp(command,"s")==0){
-				Complex(re1, im1).sub(Complex(re2, im2)).print();
-			}
-			else if(strcmp(command,"m")==0){
-				Complex(re1, im1).mul(Complex(re2, im2)).print();
-			}
-			else if(strcmp(command,"d")==0){
-				Complex(re1, im1).div(Complex(re2, im2)).print();
-			}
-			else if(strcmp(command,"e")==0){
-				std::cout << Complex(re1, im1).equ(Complex(re2, im2)) << std::endl;
-			}
-			else if(strcmp(command,"bth")==0){
-				std::cout << Complex(re1, im1).bth(Complex(re2, im2)) << std::endl;
-			}
-			else{
+		if((static_cast<std::string>(command) == "dbn") || (static_cast<std::string>(command) == "m")) {
+			if((get_value<long double>(input_real_number) != VALID_INPUT) || (input_real_number < 0)) {
+				do ch=getchar(); while((ch != EOF) && (ch != '\n'));
 				std::cout << "wrong input" << std::endl;
-				while((ch = getchar()) != '\n');
+				if(ch == EOF) return 0;
+				continue;
 			}
+		} else if((get_value<unsigned long long>(funt2n) != VALID_INPUT) || (get_value<unsigned int>(shil2n) != VALID_INPUT) || (get_value<unsigned int>(penny2n) != VALID_INPUT)) {
+			do ch = getchar(); while((ch != EOF) && (ch != '\n'));
+			std::cout << "wrong input" << std::endl;
+			if(ch == EOF) return 0;
+			continue;
 		}
+
+		conv_penny = (penny1n)/12;
+		bm1.penny = (penny1n)%12;
+		conv_shil = (shil1n+conv_penny)/20;
+		bm1.shil = (shil1n+conv_penny)%20;
+		bm1.funt = funt1n+conv_shil;
+
+		conv_penny = (penny2n)/12;
+		bm2.penny = (penny2n)%12;
+		conv_shil = (shil2n+conv_penny)/20;
+		bm2.shil = (shil2n+conv_penny)%20;
+		bm2.funt = funt2n+conv_shil;
+
+		bm1.print();
+		std::cout << '\t' << command << '\t';
+		if((static_cast<std::string>(command) == "dbn") || (static_cast<std::string>(command) == "m")) {
+			std::cout << input_real_number << '\t';
+		} else {
+			bm2.print();
+		}
+		std::cout << '\t' << '=' << '\t';
+
+
+		if((static_cast<std::string>(command) == "dbn") || (static_cast<std::string>(command) == "m")) {
+			if((input_real_number == 0) && (static_cast<std::string>(command) == "dbn"))
+				std::cout << "division by 0 is forbidden";
+			else bm1.exec(command, input_real_number).print();
+		} else if (static_cast<std::string>(command) == "lth") {
+			std::cout << (bm1<bm2);
+		} else if (static_cast<std::string>(command) == "d") {
+			std::cout << (bm1/bm2);
+		} else {
+			bm1.exec(command, bm2).print();
+		}
+		std::cout << std::endl;
+
 	} while(true);
 	return 0;
 }
